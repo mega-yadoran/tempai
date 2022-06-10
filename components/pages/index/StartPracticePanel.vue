f826191d14de3c9ef9c010fb3f36cf1b80685631
mega-yadoran <yosuke.tanaka.my@gmail.com>
2022-06-11 03:40:09 +0900

    [mod] 画面サイズ調整

--- components/pages/index/StartPracticePanel.vue
+++ components/pages/index/StartPracticePanel.vue
@@ -1,21 +1,21 @@
 <template>
-  <v-card class="px-3 px-md-5 py-3 py-md-6 text-center">
+  <v-card class="px-3 px-sm-8 py-3 py-sm-6 text-center">
     <div class="text-h5">練習モード</div>
 
     <v-divider class="my-3" />
 
     <!-- ルール -->
     <ul class="text-left mt-2">
-      <li class="my-1">
+      <li class="my-2">
         清一色のテンパイ形が表示されるので、何待ちか当ててください
       </li>
-      <li class="my-1">正解するまで何度でも回答できます</li>
-      <li class="my-1">
+      <li class="my-2">正解するまで何度でも回答できます</li>
+      <li class="my-2">
         どうしてもわからないときは
         <v-btn color="error" x-small><v-icon small>mdi-flag</v-icon></v-btn>
         を押すと答えを見ることができます
       </li>
-      <li class="my-1">無限に問題が続くので、気が済むまで練習してください！</li>
+      <li class="my-2">無限に問題が続くので、気が済むまで練習してください！</li>
     </ul>
 
     <!-- 開始ボタン -->
--- layouts/default.vue
+++ layouts/default.vue
@@ -6,20 +6,8 @@
       :height="$isSP() ? '45' : '60'"
       extension-height="35"
     >
-      <v-container class="d-flex align-center px-0">
-        <v-btn icon small @click="howToModalOpen = true">
-          <v-icon>mdi-help-circle-outline</v-icon>
-        </v-btn>
-        <v-spacer />
-
-        <span class="text-h6 text-sm-h4">清一色 待ち当て</span>
-        <v-spacer></v-spacer>
-        <v-btn icon small class="mr-md-2" @click="settingModalOpen = true">
-          <v-icon>mdi-cog-outline</v-icon>
-        </v-btn>
-        <v-btn icon small @click="statisticsModalOpen = true">
-          <v-icon>mdi-account</v-icon>
-        </v-btn>
+      <v-container class="text-center px-0">
+        <div class="text-h6 text-sm-h4">清一色待ち当て特訓</div>
       </v-container>
       <template #extension>
         <v-tabs centered height="30px">
@@ -58,8 +46,8 @@ onMounted(() => {
 
 <style lang="scss" scoped>
 .container {
-  @media (min-width: 550px) {
-    width: 530px;
+  @media (min-width: 600px) {
+    width: 580px;
   }
   .v-btn--active::before {
     display: none;
